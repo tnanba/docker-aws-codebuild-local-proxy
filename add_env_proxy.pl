462da9a48a8d8cd60c8b5c885b9ff6c3f3c78dc5
NANBA Toshiaki <t.nanba@gmail.com>
2018-05-10 15:20:11 +0900

    Exclude service link names from proxy.

--- add_env_proxy.pl
+++ add_env_proxy.pl
@@ -8,9 +8,30 @@ sub main {
 	local $/ = undef;
 	my $yml = Load(<STDIN>);
 
-	foreach my $service (qw/agent build/) {
-		my $env = $yml->{services}->{$service}->{environment};
+	foreach my $service_name (qw/agent build/) {
+
+		my $service = $yml->{services}->{$service_name};
+		my $env = $service->{environment};
+
+		# Add proxy configuration to "services/(agent|build)/environment"
 		push(@$env, qw/http_proxy https_proxy HTTP_PROXY HTTPS_PROXY/);
+
+		# Exclude link names from proxy
+		my $links = $service->{links};
+		my @link_names;
+
+		foreach my $link (@$links) {
+			if ($link =~ /^(.+):(.+)$/) {
+				push(@link_names, $2);
+			} else {
+				push(@link_names, $link);
+			}
+		}
+
+		if (scalar(@link_names) != 0) {
+			my $no_proxy = join(',', @link_names);
+			push(@$env, "$_=$no_proxy") foreach qw/no_proxy NO_PROXY/;
+		}
 	}
 
 	print Dump($yml);
